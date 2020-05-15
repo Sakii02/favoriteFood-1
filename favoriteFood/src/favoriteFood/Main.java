2f62f2d0c6168edb5990306fee5189f4f952f0ec
nanagin <nanagin@192.168.10.3>
2020-05-15 20:11:44 +0900

    貢献しました。（さき）

--- favoriteFood/src/favoriteFood/Main.java
+++ favoriteFood/src/favoriteFood/Main.java
@@ -5,7 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("好きな食べ物：寿司");
+		System.out.println("好きな食べ物：さくらんぼ");
 		System.out.println("理由：色々なネタがあって選ぶのが楽しいし健康にもいいので");
+		System.out.println("理由：甘くて美味しいからです。");
 
 	}
 
