1a44f6e0dcce38af803b9de41938d386217861d5
GingaSystem <37409683+GingaSystem@users.noreply.github.com>
2020-05-15 18:08:11 +0900

    最初のプログラム

---
+++ favoriteFood/src/favoriteFood/Main.java
@@ -0,0 +1,12 @@
+package favoriteFood;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("好きな食べ物：寿司");
+		System.out.println("理由：色々なネタがあって選ぶのが楽しいし健康にもいいので");
+
+	}
+
+}
